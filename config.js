43e5a3e47e006950919aecabb666464372df62c5
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-25 16:12:13 +0330

    feat: add responsive Persian app download landing page

---
+++ config.js
@@ -0,0 +1,6 @@
+// پس از انتشار در فروشگاه‌ها، فقط آدرس‌های زیر را ویرایش کنید.
+window.NATIJEH_DOWNLOADS = {
+  direct: "https://github.com/sahandse/natijeh/releases/latest",
+  bazaar: "",
+  myket: ""
+};
